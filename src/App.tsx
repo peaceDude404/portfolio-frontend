db2a1eb83cb54503f3825d4b669d818befb8e64d
Aman Singh <amansingh@Amans-MacBook-Air.local>
2024-12-01 19:00:58 +0530

    NavBar, Experience and Home Page Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useRef } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NavigationBar from './components/NavigationBar'
+import './App.css';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Experience from './components/Experience';
+import {HashLink as Link} from "react-router-hash-link";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const experienceRef = useRef<HTMLDivElement>(null);
+  const homeRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
    <>
-   <div>Hello ...</div>
+   <Router>
+      <NavigationBar onPressHeader={() => scrollToSection(homeRef)} onPressExperience={() => scrollToSection(experienceRef)}/>
+      <div className="content">
+        <Header headerRef={homeRef}></Header>
+        <Experience experienceRef={experienceRef}></Experience>
+      </div>
+    </Router>
    </>
   )
 }
