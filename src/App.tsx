1f24db521dc8fccb823faf9454c218cb09a6cf2e
Aman Singh <amansingh@Amans-MacBook-Air.local>
2024-12-01 13:21:30 +0530

    added the boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <div>Hello ...</div>
+   </>
+  )
+}
+
+export default App
