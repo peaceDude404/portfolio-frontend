aec9a01b11ded774df2de460c43ded0945695520
Aman Singh <amansingh@Amans-MacBook-Air.local>
2024-12-07 18:55:15 +0530

    contact and footer added , removed hover effect on skill

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import Header from './components/Header';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Experience from './components/Experience';
 import Skill from './components/Skill';
+import Contact from './components/Contact';
 
 function App() {
 
   const experienceRef = useRef<HTMLDivElement>(null);
   const homeRef = useRef<HTMLDivElement>(null);
   const skillRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
 
   const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
@@ -20,11 +22,12 @@ function App() {
   return (
    <>
    <Router>
-      <NavigationBar onPressSkill={() => scrollToSection(skillRef)} onPressHeader={() => scrollToSection(homeRef)} onPressExperience={() => scrollToSection(experienceRef)}/>
+      <NavigationBar onPressContact={() => scrollToSection(contactRef )} onPressSkill={() => scrollToSection(skillRef)} onPressHeader={() => scrollToSection(homeRef)} onPressExperience={() => scrollToSection(experienceRef)}/>
       <div className="content">
         <Header headerRef={homeRef}></Header>
         <Experience experienceRef={experienceRef}></Experience>
         <Skill skillRef={skillRef}></Skill>
+        <Contact contactRef={contactRef}></Contact>
       </div>
     </Router>
    </>
